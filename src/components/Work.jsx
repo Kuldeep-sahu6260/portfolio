ff3c4cd0474267d93df47c8fe8c725ae74aedd44
Kuldeep-sahu6260 <79197859+Kuldeep-sahu6260@users.noreply.github.com>
2022-07-13 22:21:36 +0530

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import WorkImg from '../assets/workImg.png';
+import share from '../assets/share.png';
 import realEstate from '../assets/realestate.png';
 import pwa from '../assets/netfix.png';
 import income from '../assets/income.png';
@@ -8,7 +9,7 @@ import video from '../assets/video.png';
 
 const Work = () => {
   return (
-    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f] pt-40'>
+    <div name='work' className='w-full text-gray-300 bg-[#0a192f] '>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
@@ -21,17 +22,17 @@ const Work = () => {
         <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
 
             {/* Grid Item */}
-          <div
-            style={{ backgroundImage: `url(${WorkImg})` }}
+            <div
+            style={{ backgroundImage: `url(${share})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
             {/* Hover Effects */}
             <div className='opacity-0 group-hover:opacity-100'>
               <span className='text-2xl font-bold text-white tracking-wider'>
-                News Application
+                Pintrest App
               </span>
               <div className='pt-8 text-center'>
-                <a href='https://news-app-my.netlify.app/'>
+                <a href='https://pintrestkk.netlify.app/'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
@@ -154,6 +155,29 @@ const Work = () => {
                   </button>
                 </a>
                 <a href='https://github.com/Kuldeep-sahu6260/client_vider_chat_app'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${WorkImg})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                News Application
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://news-app-my.netlify.app/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://github.com/Kuldeep-sahu6260/recoderApp'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
